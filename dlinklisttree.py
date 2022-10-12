978f3232eeff124e2008576379504f6bd8fd68f0
abin222 <abin.joseph222@gmail.com>
2022-10-12 14:55:12 +0530

    12/10

--- dlinklisttree.py
+++ dlinklisttree.py
@@ -1,8 +1,55 @@
 class Tree:
     # Node definition
     class Node:
-        def __init__(self,data,parent=None,Child1=None,Child2=None,):
+        def __init__(self,data):
             self.data=data
-            self.parent=parent
-            self.child1=Child1
-            self.child2=Child2
+            self.parent=None
+            self.lc=None     #left child
+            self.rc=None #right child
+    def __init__(self):
+        self.root =None
+    def constructttree(self,expr):
+        i=0
+        lc=None
+        while i< len(expr):
+            print(expr[i])
+            if lc==None:
+                print("lc is none")
+                lc=self.Node(expr[i])
+                i+=1
+                op=self.Node(expr[i])
+                i+=1
+                rc=self.Node(expr[i])
+                op.lc=lc
+                op.rc=rc
+                op.parent=None
+                lc.parent=op
+                rc.parent=op
+                self.root=op
+                print("i=",i)
+            else:
+                print("lc id not none")
+                lc=op
+                op=self.Node(expr[i])
+                i+=1
+                rc=self.Node(expr[i])
+                op.parent=None
+                op.lc=lc
+                op.rc=rc
+                lc.parent=op
+                rc.parent=op
+                self.root=op
+                print("ii=",i)
+            i+=1
+    def traverseTree(self,root):
+         if root.data:
+             print(self.traverseTree(root.lc))
+             print(root.data)
+             print(self.traverseTree(root.rc))
+if __name__ == "__main__":
+	tree=Tree()
+	tree.constructttree("a+b*c/d")
+	# print(tree.root.data)
+	tree.traverseTree(tree.root)
+             
+        
